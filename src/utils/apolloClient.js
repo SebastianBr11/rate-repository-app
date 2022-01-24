03a9446ef9e4fe9bf4055c232b085224dab3c383
Sebastian Breit <flockean11@gmail.com>
2022-01-24 07:58:43 +0000

    Exercise 10.12: environment variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'https://4000-sebastianbr11-ratereposi-yp15vrzbzb8.ws-eu27.gitpod.io/graphql',
+  uri: Constants.manifest.extra.apolloUri,
 });
 
 const createApolloClient = () => {
