821f4890780b0acdebaa7a81534e819913b11c92
Sebastian Breit <flockean11@gmail.com>
2022-01-24 07:36:24 +0000

    setup

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    // Replace the IP address part with your own IP address!
+    const response = await fetch(
+      'https://5000-sebastianbr11-ratereposi-yp15vrzbzb8.ws-eu27.gitpod.io/api/repositories'
+    );
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
